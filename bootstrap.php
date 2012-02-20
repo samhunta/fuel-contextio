99267531d2944d032b83ad9f2d89abb98599f5f1
Dan Harper <intouch@danharper.me>
2012-02-20 14:25:12 +0000

    Improve loading of library
    Allows for `ContextIO::listAccounts()->getData()` instead of having the
    explicitly get the instance first with
    `ContextIO::instance()->listAccounts()->getData()`

--- bootstrap.php
+++ bootstrap.php
@@ -12,26 +12,27 @@
 Autoloader::add_core_namespace('ContextIO');
 
 Autoloader::add_classes(array(
-	'ContextIO\\ContextIO'             => __DIR__.'/classes/contextio.php',
-	'ContextIO\\ContextIOResponse'      => __DIR__.'/classes/contextio/response.php',
+	'ContextIO\\ContextIO'         => __DIR__.'/classes/contextio.php',
+	'ContextIO\\ContextIOClient'   => __DIR__.'/classes/contextio/client.php',
+	'ContextIO\\ContextIOResponse' => __DIR__.'/classes/contextio/response.php',
 
 
 	/**
 	 * @todo Move this to a seperate package
 	 */
-	'ContextIO\\OAuthException'        => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthConsumer'         => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthToken'            => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthSignatureMethod'  => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthRequest'          => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthServer'           => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthDataStore'        => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthUtil'             => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthException'       => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthConsumer'        => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthToken'           => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthSignatureMethod' => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthRequest'         => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthServer'          => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthDataStore'       => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthUtil'            => __DIR__.'/classes/contextio/oauth.php',
 	
-	'ContextIO\\OAuthSignatureMethod_HMAC_SHA1'  => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthSignatureMethod_PLAINTEXT'  => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthSignatureMethod_RSA_SHA1'   => __DIR__.'/classes/contextio/oauth.php',
-	'ContextIO\\OAuthSignatureMethod_HMAC_SHA1'  => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthSignatureMethod_HMAC_SHA1' => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthSignatureMethod_PLAINTEXT' => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthSignatureMethod_RSA_SHA1'  => __DIR__.'/classes/contextio/oauth.php',
+	'ContextIO\\OAuthSignatureMethod_HMAC_SHA1' => __DIR__.'/classes/contextio/oauth.php',
 ));
 
 /* End of file bootstrap.php */
